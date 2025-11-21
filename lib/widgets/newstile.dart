491bd861925d187c7c90a5887bc15c9a8b99c65a
Mahmoud Ayman <mahmoudayman202030@gmail.com>
2025-11-21 13:07:01 +0200

    Update image URL in Newstile widget

--- lib/widgets/newstile.dart
+++ lib/widgets/newstile.dart
@@ -10,7 +10,7 @@ class Newstile extends StatelessWidget {
         ClipRRect(
           borderRadius: BorderRadiusGeometry.all(Radius.circular(8)),
           child: Image.network(
-            " https://pub.dev/documentation/blur_container/latest/",
+            "https://www.google.com/imgres?q=photos&imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1495745966610-2a67f2297e5e%3Ffm%3Djpg%26q%3D60%26w%3D3000%26ixlib%3Drb-4.1.0%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG9ncmFwaGVyfGVufDB8fDB8fHww&imgrefurl=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fphotographer&docid=8ncfNjXWb262-M&tbnid=7gc14Qafc2ivEM&vet=12ahUKEwjBtoDWjIORAxWrVkEAHU-HGTIQM3oECCQQAA..i&w=3000&h=4500&hcb=2&ved=2ahUKEwjBtoDWjIORAxWrVkEAHU-HGTIQM3oECCQQAA",
           ),
         ),
         const SizedBox(height: 4),
